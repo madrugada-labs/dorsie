75e4ae1904ab835d11ae285450168b8db1efe9d9
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2022-06-08 22:16:16 +0200

    avoid loop gotcha

--- data_fetcher.go
+++ data_fetcher.go
@@ -33,25 +33,31 @@ func (df *DataFetcher) GetJobsPublic() (*tview.List, error) {
 	}
 	jobsListUI := tview.NewList()
 	for i, job := range query.JobsPublic {
+		// copy the job.ID variable to avoid the GO gotcha :) in for loops: https://kkentzo.github.io/2021/01/21/golang-loop-variable-gotcha/
+		jobID := job.ID.(string)
 		jobsListUI = jobsListUI.AddItem(
 			fmt.Sprintf("[::b] [%s - %s USD][-:-:-] %s @ %s", humanize.Comma(int64(job.MaxSalary)), humanize.Comma(int64(job.MaxSalary)), job.Title, job.Company.Name),
 			fmt.Sprintf("[green::]%s[-:-:-], [blue::]%s[-:-:-]", job.Experience, job.Field),
 			rune(i),
 			func() {
-				url := fmt.Sprintf("https://dorse.io/job/%s", job.ID)
-				switch runtime.GOOS {
-				case "linux":
-					err = exec.Command("xdg-open", url).Start()
-				case "darwin":
-					err = exec.Command("open", url).Start()
-				case "windows":
-					err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-				default:
-					err = fmt.Errorf("unsupported platform")
-				}
+				openJob(jobID)
 			},
 		)
 	}
 	return jobsListUI, nil
 
+}
+
+func openJob(jobAdID string) {
+	url := fmt.Sprintf("https://dorse.io/job/%s", jobAdID)
+	switch runtime.GOOS {
+	case "linux":
+		_ = exec.Command("xdg-open", url).Start()
+	case "darwin":
+		_ = exec.Command("open", url).Start()
+	case "windows":
+		_ = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	default:
+		_ = fmt.Errorf("unsupported platform")
+	}
 }
